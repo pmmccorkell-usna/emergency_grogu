76c22f6abf3204b2f5da0b0e484faf232aeaba52
pmmccorkell-usna <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-12-22 12:05:11 -0500

    Create CHANGE_ME_secrets.py

---
+++ CHANGE_ME_secrets.py
@@ -0,0 +1,6 @@
+ada_user = 'insert_your_adafruit_username'
+ada_key = 'insert_your_adafruit_key'
+ada_topic = 'your_feed_name'
+
+server1 = 'private IP of printer'
+server2 = 'public IP of printer : forwarded port of printer'
