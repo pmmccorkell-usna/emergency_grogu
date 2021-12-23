038694dbb7806cc90301cab6f78c849dfe63b1b7
pmmccorkell-usna <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-12-23 08:24:18 -0500

    Update emergency.py

--- emergency.py
+++ emergency.py
@@ -32,41 +32,45 @@ def emergency_shutoff(server):
 
 def kill_it_all():
 	global server1, server2
-	print('entered kill_it_all')
 	if (not emergency_shutoff(server1)):
 		sleep(1)
 		emergency_shutoff(server2)
 
+# Function to be called when topic is received.
+# Must be in the format of func(topic,message)
 def check_emergency(topic,message):
 	print(topic)
 	print(message.decode())
 	if (message.decode()=='1'):
 		kill_it_all()
 
+# Dictionary of topics and associated callback functions for mqtt subscription.
+# Each entry must be in the format of 'topic : callback_function'
 subscription_dict = {
 	ada_topic:check_emergency
 }
 
 def connect_ada():
-	global ada, connected
+	global ada
+	# Instantiate paho-mqtt wrapper class
 	ada = mqttClass.mqttClass(host_IP='io.adafruit.com',username=ada_user, key=ada_key, subscriptions = subscription_dict)
 	ada.connect()
+	# Registers the reconnect_ada function to be run when paho-mqtt detects a disconnect
 	ada.client.on_disconnect = reconnect_ada
-	connected = 1
 
+# If connection is lost, run this function to reconnect.
+# Must be in the format of func(client,userdata,rc)
 def reconnect_ada(client,userdata,rc):
-	global ada, connected
-	connected = 0
+	global ada
 	ada.mqtt_terminate()
 	sleep(3)
 	connect_ada()
 
 
 def main():
-	global connected
-	connected = 0
 	connect_ada()
 	while(1):
+		# Everything is handled by interrupts and callbacks, so just keep the program alive with sleeps
 		sleep(1)
 
 if __name__ == '__main__':
